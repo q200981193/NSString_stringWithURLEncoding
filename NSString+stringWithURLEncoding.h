e8556db23145524bbfc9d799d8e955567009ef8b
Jonas Carlsson <jonas@jonasonline.se>
2012-03-21 22:28:29 +0100

    initial commit

---
+++ NSString+stringWithURLEncoding.h
@@ -0,0 +1,13 @@
+//
+//  NSString+stringWithURLEncoding.h
+//  NSString_stringWithURLEncoding
+//
+//  Created by Jonas Carlsson on 2012-03-21.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface NSString (stringWithURLEncoding)
+
+@end
---
+++ NSString+stringWithURLEncoding.m
@@ -0,0 +1,13 @@
+//
+//  NSString+stringWithURLEncoding.m
+//  NSString_stringWithURLEncoding
+//
+//  Created by Jonas Carlsson on 2012-03-21.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSString+stringWithURLEncoding.h"
+
+@implementation NSString (stringWithURLEncoding)
+
+@end
